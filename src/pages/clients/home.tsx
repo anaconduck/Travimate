8689a302da3a3984cd70a4de2acf9004d6417cea
anaconduck <wahyukusuma29@gmail.com>
2024-01-30 04:39:57 +0700

    web-slider

--- src/pages/clients/home.tsx
+++ src/pages/clients/home.tsx
@@ -2,11 +2,13 @@ import LayoutClient from "../../components/layout-clients/layout"
 import Hero from "../../components/layout-clients/part/hero"
 import DownloadApp from "../../section/clients/download-app"
 import WhyUs from "../../section/clients/why-us"
+import WebSlider from "../../section/clients/web-slider"
 
 const HomePage = () => {
   return (
     <LayoutClient>
         <Hero />
+        <WebSlider/>
         <WhyUs />
         <DownloadApp />
     </LayoutClient>
