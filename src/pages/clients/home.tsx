d3b37cc26ab3144cc05771cffcf10ad79fbe00c6
anaconduck <wahyukusuma29@gmail.com>
2024-02-02 20:51:42 +0700

    slider-fix

--- src/pages/clients/home.tsx
+++ src/pages/clients/home.tsx
@@ -5,10 +5,20 @@ import WhyUs from "../../section/clients/why-us"
 import WebSlider from "../../section/clients/web-slider"
 
 const HomePage = () => {
+
+  let slides = [
+    "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
+    "https://wallpapercave.com/wp/wp3386769.jpg",
+    "https://wallpaperaccess.com/full/809523.jpg",
+    "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
+  ];
+
   return (
     <LayoutClient>
         <Hero />
-        <WebSlider/>
+        <div className="w-[80%] m-auto pt-11">
+          <WebSlider slides={slides} />
+        </div>
         <WhyUs />
         <DownloadApp />
     </LayoutClient>
