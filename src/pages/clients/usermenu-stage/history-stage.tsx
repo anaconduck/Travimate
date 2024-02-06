b671843236362fc6f65361b249b8ecd97fe12d28
Hassanjadi <hassanjadi25@gmail.com>
2024-02-06 18:32:00 +0700

    feat: detail e-ticket

--- src/pages/clients/usermenu-stage/history-stage.tsx
+++ src/pages/clients/usermenu-stage/history-stage.tsx
@@ -2,138 +2,23 @@ import React from "react";
 import { HistoryIllustration } from "../../../assets/ilustrations";
 import EmptyState from "../../../components/layout-clients/empty-state/empty-state";
 import { GoArrowSwitch } from "react-icons/go";
-import HistoryCardBlue from "../../../components/layout-clients/part/history-card-blue";
-import HistoryCardOrange from "../../../components/layout-clients/part/history-card-orange";
-import InformasiPenumpang from "../../../components/layout-clients/part/informasi-penumpang";
+import RoundTripOnePerson from "../../../components/layout-clients/part/round-trip-one-person";
+import RoundTripMorePerson from "../../../components/layout-clients/part/round-trip-more-person";
+import OneWay from "../../../components/layout-clients/part/one-way";
 
 interface IHistoryStage {
   setRiwayat: (state: string) => void;
   riwayat: string;
+  type: "oneWay" | "roundTrip";
+  passengers: number;
 }
 
 const HistoryStage = ({ setRiwayat, riwayat }: IHistoryStage) => {
   const renderDetails = (
     <>
-      <div className="bg-white w-full rounded-[27.51px] relative">
-        <div className="bg-[#3E7BFA] w-full rounded-t-[27.51px] pb-8">
-          <div className="flex p-8 gap-4">
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="48"
-                height="48"
-                viewBox="0 0 48 48"
-                fill="none"
-              >
-                <path
-                  d="M31.41 33.18L22.25 24L31.41 14.82L28.59 12L16.59 24L28.59 36L31.41 33.18Z"
-                  fill="white"
-                />
-              </svg>
-            </span>
-            <div className="flight text-white font-sans">
-              <h2 className="text-3xl font-bold leading-10">
-                Jakarta (CGK) - Bali (DPS)
-              </h2>
-              <h3 className="text-2xl font-semibold leading-10">Order ID: 1</h3>
-            </div>
-          </div>
-        </div>
-
-        {/* Circle */}
-        <div className="w-[116px] h-[116px] bg-[#3E7BFA] rounded-full absolute top-20 left-80 z-10"></div>
-
-        {/* Booking */}
-        <div className="bg-white w-full rounded-[27.51px] py-10 mt-[-30px]">
-          <div className="w-full p-8 font-sans text-[#757575]">
-            <p className="text-2xl font-bold leading-10">Kode booking</p>
-            <div className="flex gap-2 items-center">
-              <h1 className="text-[32px] font-bold leading-10 text-[#3E7BFA]">
-                JB8TN4
-              </h1>
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 32 32"
-                  fill="none"
-                >
-                  <path
-                    d="M21.9999 1.33398H5.99992C4.53325 1.33398 3.33325 2.53398 3.33325 4.00065V22.6673H5.99992V4.00065H21.9999V1.33398ZM25.9999 6.66732H11.3333C9.86658 6.66732 8.66658 7.86732 8.66658 9.33398V28.0006C8.66658 29.4673 9.86658 30.6673 11.3333 30.6673H25.9999C27.4666 30.6673 28.6666 29.4673 28.6666 28.0006V9.33398C28.6666 7.86732 27.4666 6.66732 25.9999 6.66732ZM25.9999 28.0006H11.3333V9.33398H25.9999V28.0006Z"
-                    fill="#3E7BFA"
-                  />
-                </svg>
-              </span>
-            </div>
-            <h2 className="text-2xl font-bold leading-10">PNR: JB8TN4</h2>
-          </div>
-          {/* Line */}
-          <span className="flex justify-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="690"
-              height="4"
-              viewBox="0 0 795 4"
-              fill="none"
-            >
-              <path
-                d="M2 2L793 2.00007"
-                stroke="#EDEDED"
-                strokeWidth="3"
-                strokeLinecap="round"
-              />
-            </svg>
-          </span>
-
-          <HistoryCardBlue />
-
-          {/* Line */}
-          <span className="flex justify-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="690"
-              height="4"
-              viewBox="0 0 795 4"
-              fill="none"
-            >
-              <path
-                d="M2 2L793 2.00007"
-                stroke="#EDEDED"
-                strokeWidth="3"
-                strokeLinecap="round"
-              />
-            </svg>
-          </span>
-
-          <HistoryCardOrange />
-
-          {/* Line */}
-          <span className="flex justify-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="690"
-              height="4"
-              viewBox="0 0 795 4"
-              fill="none"
-            >
-              <path
-                d="M2 2L793 2.00007"
-                stroke="#EDEDED"
-                strokeWidth="3"
-                strokeLinecap="round"
-              />
-            </svg>
-          </span>
-
-          <div className="p-8">
-            <h1 className="text-[#505050] text-[32px] font-bold leading-10 mb-8">
-              Informasi Penumpang
-            </h1>
-            <InformasiPenumpang />
-          </div>
-        </div>
-      </div>
+      <OneWay />
+      <RoundTripOnePerson />
+      <RoundTripMorePerson />
     </>
   );
 
