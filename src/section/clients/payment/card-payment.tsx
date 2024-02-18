01c4430b10c94452a4169944615fd7589a13a0ce
davidnasrulloh <davidkrb52@gmail.com>
2024-02-18 20:02:15 +0700

    fix: add render payment details

--- src/section/clients/payment/card-payment.tsx
+++ src/section/clients/payment/card-payment.tsx
@@ -1,6 +1,9 @@
 import { ChangeEvent, useState } from "react";
 import { PaymentDataKey } from "./payment-page-view";
+import { useSelector } from 'react-redux'
 import { Dropdown, Label, Radio, Select, TextInput } from "flowbite-react";
+import { selectDataFlighState } from "../../../store/flights/flights.slice";
+import { FlightData } from "../../../types";
 
 interface ICardPayment {
   handlePayment?: (e: any) => void;
@@ -10,6 +13,8 @@ interface ICardPayment {
 }
 
 const CardPayment = ({onSubmit, values, handleSetValueInput}:ICardPayment) => {
+
+  const dataDetailFlight = useSelector(selectDataFlighState)
 
   const [checked, setChecked] = useState<"bca" | "credit">("bca");
 
@@ -112,79 +117,85 @@ const CardPayment = ({onSubmit, values, handleSetValueInput}:ICardPayment) => {
   )
 
   const renderCardRight = (
-    <div className="flex flex-col justify-between h-full">
-      <div className="">
-        <div className="text-lg text-slate-600">Order ID : 1</div>
-        <div className="font-bold text-xl text-slate-700 capitalize mt-2">
-          Ringkasan Pesanan
-        </div>
-        <div className="mt-2 text-lg text-slate-600">
-          02-02-2024 - 04-02-2024
-        </div>
-        <div className="flex justify-start items-center gap-2 mt-2 text-lg text-slate-600">
-          <div>CGK</div>
-          <div>
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M19.875 9.28125C20.0312 9.125 20.0312 8.90625 19.875 8.75L16.875 5.75C16.625 5.5 16.2188 5.6875 16.2188 6V8.25H4.375C4.15625 8.25 4 8.4375 4 8.625V9.375C4 9.59375 4.15625 9.75 4.375 9.75H16.2188V12.0312C16.2188 12.3438 16.625 12.5312 16.875 12.2812L19.875 9.28125ZM4.09375 15.2812L7.09375 18.2812C7.34375 18.5312 7.75 18.3438 7.75 18.0312V15.75H19.625C19.8125 15.75 20 15.5938 20 15.375V14.625C20 14.4375 19.8125 14.25 19.625 14.25H7.75V12C7.75 11.6875 7.34375 11.5 7.09375 11.75L4.09375 14.75C3.9375 14.9062 3.9375 15.125 4.09375 15.2812Z"
-                fill="#333333"
-              />
-            </svg>
-          </div>
-          <div>DPS</div>
-        </div>
-        <div className="flex justify-center items-center text-blue-600 font-bold my-5 text-lg">
-          <button>Detail</button>
-        </div>
-      </div>
-      <div className="border-t-2 border-slate-200 pt-4">
-        <div className="flex flex-col gap-2 mb-4">
-            <label className="font-[Open Sans] text-lg font-semibold text-[#727070]">Masukkan kode promo</label>
-            <TextInput title='promo_code. Handphone' name="promo_code" sizing='md' onChange={(e: any)=> {
-              handleSetValueInput({
-                key: 'promo_code',
-                values: e.target.value
-              })
-            }} placeholder="Promo Code" type='text'/>
-        </div>
-        <div>
-          <button
-            type="submit"
-            className="w-full mt-4 font-bold bg-blue-500 text-white p-2 rounded-3xl hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-          >
-            Terapkan
-          </button>
-        </div>
-      </div>
-      <div className="border-t-2 border-slate-200 mt-6">
-        <div className="flex justify-between items-center mt-2">
-          <div className="text-sm">Total Pembayaran</div>
-          <div className="flex justify-end items-center">
-            <div className="font-bold text-2xl">Rp1.368.000</div>
-            <div>
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7.41 8.29492L12 12.8749L16.59 8.29492L18 9.70492L12 15.7049L6 9.70492L7.41 8.29492Z"
-                  fill="#505050"
-                />
-              </svg>
+    <>
+      {
+        dataDetailFlight?.flights?.map((item: FlightData)=>(
+          <div className="flex flex-col justify-between h-full">
+            <div className="">
+              <div className="text-lg text-slate-600">Order ID : 1</div>
+              <div className="font-bold text-xl text-slate-700 capitalize mt-2">
+                Ringkasan Pesanan
+              </div>
+              <div className="mt-2 text-lg text-slate-600">
+                02-02-2024 - 04-02-2024
+              </div>
+              <div className="flex justify-start items-center gap-2 mt-2 text-lg text-slate-600">
+                <div>CGK</div>
+                <div>
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M19.875 9.28125C20.0312 9.125 20.0312 8.90625 19.875 8.75L16.875 5.75C16.625 5.5 16.2188 5.6875 16.2188 6V8.25H4.375C4.15625 8.25 4 8.4375 4 8.625V9.375C4 9.59375 4.15625 9.75 4.375 9.75H16.2188V12.0312C16.2188 12.3438 16.625 12.5312 16.875 12.2812L19.875 9.28125ZM4.09375 15.2812L7.09375 18.2812C7.34375 18.5312 7.75 18.3438 7.75 18.0312V15.75H19.625C19.8125 15.75 20 15.5938 20 15.375V14.625C20 14.4375 19.8125 14.25 19.625 14.25H7.75V12C7.75 11.6875 7.34375 11.5 7.09375 11.75L4.09375 14.75C3.9375 14.9062 3.9375 15.125 4.09375 15.2812Z"
+                      fill="#333333"
+                    />
+                  </svg>
+                </div>
+                <div>DPS</div>
+              </div>
+              <div className="flex justify-center items-center text-blue-600 font-bold my-5 text-lg">
+                <button>Detail</button>
+              </div>
+            </div>
+            <div className="border-t-2 border-slate-200 pt-4">
+              <div className="flex flex-col gap-2 mb-4">
+                  <label className="font-[Open Sans] text-lg font-semibold text-[#727070]">Masukkan kode promo</label>
+                  <TextInput title='promo_code. Handphone' name="promo_code" sizing='md' onChange={(e: any)=> {
+                    handleSetValueInput({
+                      key: 'promo_code',
+                      values: e.target.value
+                    })
+                  }} placeholder="Promo Code" type='text'/>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full mt-4 font-bold bg-blue-500 text-white p-2 rounded-3xl hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                >
+                  Terapkan
+                </button>
+              </div>
+            </div>
+            <div className="border-t-2 border-slate-200 mt-6">
+              <div className="flex justify-between items-center mt-2">
+                <div className="text-sm">Total Pembayaran</div>
+                <div className="flex justify-end items-center">
+                  <div className="font-bold text-2xl">Rp1.368.000</div>
+                  <div>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M7.41 8.29492L12 12.8749L16.59 8.29492L18 9.70492L12 15.7049L6 9.70492L7.41 8.29492Z"
+                        fill="#505050"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+        ))
+      }
+    </>
   ) 
 
   return (
